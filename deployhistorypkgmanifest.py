2c97ea3b1bc671d9e00b8f9e3906210899674e2c
Nukley <minecraftfreak303@gmail.com>
2023-10-11 16:15:04 -0400

    Bug Fixes in the loop

--- deployhistorypkgmanifest.py
+++ deployhistorypkgmanifest.py
@@ -33,10 +33,12 @@ def WorkerThread():
                 latest = ClientArchiveQueue.pop(0)
             archiveURLS(latest)
 
-def archiveURLS(pkgManifest, curVersion):
+def archiveURLS(curVerArgs):
+    pkgManifest, curVersion = curVerArgs
     currentFailedFiles = []
     for v in pkgManifest:
         currentClientUrl = f"https://setup.rbxcdn.com/{curVersion}-{v}"
+        print(currentClientUrl)
         for i in range(8):
             try:
                 SaveClientNow(curVersion, v)
@@ -45,11 +47,12 @@ def archiveURLS(pkgManifest, curVersion):
                 print(f"Error: {e}")
                 open("log.txt", "a").write(f"{e}")
                 time.sleep(8)
-                try:
-                    currentFailedFiles.index(currentClientUrl)
-                except Exception as h:
-                    currentFailedFiles.append(currentClientUrl)
-                    time.sleep(1)
+                if i == 7:
+                    try:
+                        currentFailedFiles.index(currentClientUrl)
+                    except Exception as h:
+                        currentFailedFiles.append(currentClientUrl)
+                        time.sleep(1)
 
     FailedArchiveEmbed = discord.Embed(title=f"Done! Failed Archives: {len(currentFailedFiles)}", description = curVersion)
     for i in currentFailedFiles:
@@ -112,6 +115,11 @@ while True:
                     fileListEmbed.add_field(name = v, value = f"https://setup.rbxcdn.com/{version}-{v}", inline = False)
             fileListEmbed.set_image(url="https://media.discordapp.com/attachments/976287740771598379/1105135729744543776/clientsearch_folder2.png")
             Webhook.send(embed=fileListEmbed)
+            pkgManifest.append("rbxManifest.txt")
+            if CurrentBinaryType == BinaryTypes[0]:
+                pkgManifest.append("RobloxPlayerLauncher.exe")
+            if CurrentBinaryType == BinaryTypes[1]:
+                pkgManifest.append("RobloxStudioLauncherBeta.exe")
 
             with Queuelock:
                 print("LOCK")
